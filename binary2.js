4a21c62caa158b8677cb2cf4e37a1fdaf9adcfb8
citybusters <kabirteria10@gmail.com>
2017-08-01 20:51:13 +0530

    full bugs gone

--- binary2.js
+++ binary2.js
@@ -1,6 +1,8 @@
 var usertry=document.querySelector("input");
 var submit=document.querySelector("button");
 var result=document.querySelector(".result");
+var reset=document.querySelector(".newgame");
+
 submit.onclick=function(){
 var a =Number(usertry.value);
 var b=a;
@@ -8,4 +10,8 @@ var b=a;
 var b=parseInt(a).toString(2);
 result.textContent="The binary code for "+a+" is "+b;
 //document.write("The binary code for "+a+" is "+b);
+}
+reset.onclick=function(){
+usertry.value="";
+result.textContent="";
 }
